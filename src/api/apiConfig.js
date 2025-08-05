8b94bea88aae66bd0e011e1f6285cd9c5dc66cea
Abdelalim <abdelalim.study@gmail.com>
2025-08-05 00:10:00 -0400

    try

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,5 +1,5 @@
 // Production configuration
-const BASE_URL = process.env.REACT_APP_API_URL || 'mysafespacebackend-eudpc9a9c4c9h0ap.canadacentral-01.azurewebsites.net';
+const BASE_URL = process.env.REACT_APP_API_URL || 'https://mysafespacebackend-eudpc9a9c4c9h0ap.canadacentral-01.azurewebsites.net/';
 
 const apiConfig = {
   auth: `${BASE_URL}/api/auth`,
